a58aa333407b0343b6145dfcc01c5ba963319b15
Daniel Otsuka <dkotsuka@gmail.com>
2019-05-28 12:06:52 -0300

    colors.js updated

--- utils/colors.js
+++ utils/colors.js
@@ -1,1 +1,6 @@
 export const primaryColor = "#df12ab"
+export const primaryTextColor = "#ffffff"
+
+export const secondaryLightColor = "fafafa"
+export const secondaryColor = "ababab"
+export const secondaryDarkColor = "656565"
