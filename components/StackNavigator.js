3a2b2015738f10e4c5b9eda32fbbe7bb9d315d15
Daniel Otsuka <dkotsuka@gmail.com>
2019-05-28 09:32:04 -0300

    StackNavigator added

---
+++ components/StackNavigator.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createStackNavigator } from 'react-navigation'
+
+import CityList from './CityList'
+import LocationList from './LocationList'
+
+const StackNavigator = createStackNavigator(
+	{
+		CityList,
+		LocationList
+	},
+	{
+		initialRouteName: 'CityList',
+		headerMode : 'none',
+	}
+)
+
+export default StackNavigator
